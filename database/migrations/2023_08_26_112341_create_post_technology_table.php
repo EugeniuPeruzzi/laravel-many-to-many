7fd696fadaf81d908ba352a1731ba8804942045b
EugeniuPeruzzi <jaka1997@live.it>
2023-08-26 13:36:45 +0200

    created bridge table btween post and technologies

---
+++ database/migrations/2023_08_26_112341_create_post_technology_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_technology', function (Blueprint $table) {
+
+            // tabella ponte id di tecnologies
+            $table->unsignedBigInteger('post_id');
+            $table->foreign('post_id')->references('id')->on('posts');
+
+            // tabella ponte id di tecnologies
+            $table->unsignedBigInteger('technologies_id');
+            $table->foreign('technologies_id')->references('id')->on('technologies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_technology');
+    }
+};
--- resources/views/admin/technologies/index.blade.php
+++ resources/views/admin/technologies/index.blade.php
@@ -1,5 +1,61 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>Ciao Techs</h1>
+    <div class="container">
+        <div class="col">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-evenly align-items-center my-5">
+                    <h1>I nostri progetti</h1>
+                    <div class="btn-container d-flex">
+                        <div class="btn-container">
+                            <a href="{{ Route('admin.posts.create') }}"><button class="btn btn-secondary">Crea
+                                    progetto</button></a>
+                        </div>
+                        <div class="btn-container ms-4">
+                            <a href="{{ Route('admin.technologies.index') }}"><button class="btn btn-warning">Modifica le
+                                    tecnologies</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <table class="table table-stripped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Slug</th>
+                                <th>Strumenti</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($techs as $item)
+                                <tr>
+                                    <td>{{ $item->id }}</td>
+                                    <td>{{ $item->name }}</td>
+                                    <td>{{ $item->slug }}</td>
+                                    <td>
+                                        <a href="{{ Route('admin.posts.show', $item->id) }}"
+                                            class="btn btn-sm btn-primary">Show</a>
+                                        <a href="{{ Route('admin.posts.edit', $item->id) }}"
+                                            class="btn btn-sm btn-warning">Edit</a>
+                                        <form action="{{ Route('admin.posts.destroy', $item) }}"
+                                            class="d-inline-block project-delete-button"
+                                            data-project-title="{{ $item->title }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger " data-bs-toggle="modal"
+                                                data-bs-target="#staticBackdrop">
+                                                Delete
+                                        </form>
+
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    @include('admin.partials.modal_delete');
 @endsection
